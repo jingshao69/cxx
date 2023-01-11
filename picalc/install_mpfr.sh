b71884083e2f81fcaa8af2f18476468594583acd
Jing Shao <jing_shao@yahoo.com>
2023-01-10 20:18:13 -0600

    Test

--- picalc/install_mpfr.sh
+++ picalc/install_mpfr.sh
@@ -4,6 +4,8 @@ APT_PKG=libmpfr-dev
 LIB_MPFR_EXIST=$(apt list --installed | grep $APT_PKG)
 SUDO=$(which sudo)
 
+#Test
+
 if [ "$LIB_MPFR_EXIST" == "" ]; then
     $SUDO apt-get install -y $APT_PKG
 fi
